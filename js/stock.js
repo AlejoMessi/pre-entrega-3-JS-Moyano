ca9739c27a80be03360ca7762671568d44c20e33
alejo moyano <moyano887@gmail.com>
2022-12-26 19:28:33 -0300

    paginas

---
+++ js/stock.js
@@ -0,0 +1,52 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Fernet Branca",
+        tipo: "Aperitivo",
+        precio: 1200,
+        cantidad: "750 ml",
+        cantidad:0
+    },
+    {
+        id: 2,
+        nombre: "Skyy",
+        tipo: "vodka",
+        precio: 1500,
+        cantidad: "1 L",
+        cantidad:0
+    },
+    {
+        id: 3,
+        nombre: "Absolute",
+        tipo: "vodka",
+        precio: 2000,
+        cantidad: "1 L",
+        cantidad:0
+    },
+    {
+        id: 4,
+        nombre: "Gancia",
+        tipo: "aperitivo",
+        precio: 900,
+        cantidad: "1 L",
+        cantidad:0
+    },
+    
+    {
+        id: 5,
+        nombre: "Speed",
+        tipo: "energizante",
+        precio: 400,
+        cantidad: "473 ml",
+        cantidad:0
+    },
+    {
+        id: 6,
+        nombre: "Monster",
+        tipo: "energizante",
+        precio: 600,
+        cantidad: "473 ml",
+        cantidad:0
+    }
+]
